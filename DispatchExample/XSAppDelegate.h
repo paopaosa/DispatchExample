b51150806fe148da597b5652c12f23a094715fcb
Paopao sa <paopaosa@qq.com>
2013-04-07 16:07:35 +0800

    Initial Commit

---
+++ DispatchExample/XSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XSAppDelegate.h
+//  DispatchExample
+//
+//  Created by macpps on 13-4-7.
+//  Copyright (c) 2013年 ICV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DispatchExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DispatchExample
+//
+//  Created by macpps on 13-4-7.
+//  Copyright (c) 2013年 ICV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XSAppDelegate class]));
+    }
+}
